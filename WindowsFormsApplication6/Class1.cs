54c42494a941c8c400c9f8fdf9ad1a2fe7e24952
5521548 <5521548@UU045568>
2017-10-13 11:51:37 +0200

    Snel een setup van deeltje gemaakt

---
+++ WindowsFormsApplication6/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+
+namespace WindowsFormsApplication6
+{
+    class Rondje
+    {
+        private int x, y, dx, dy;
+        private Brush brush;
+    public Rondje(Brush brush, int x, int y, int dx, int dy)
+        {
+           
+            
+        }
+        public void Veranderkleur(Size hok)
+        {   
+           
+
+
+        }
+    public void TekenRondje(Graphics gr)
+        {
+            gr.FillEllipse(this.brush, this.x - 4, this.y - 4, 9, 9);
+        }
+    }
+
+}
